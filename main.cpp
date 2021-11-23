3e3f29e6f85889e325becf1f00623c37fa361613
Bob Aaldering <74597240+BobAaldering@users.noreply.github.com>
2021-11-23 18:58:03 +0100

    Small changes to the main.cpp file.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@
 int main(int argc, char* argv[]) {
     boost::program_options::options_description description("Allowed options");
     description.add_options()
-            ("help", "produce help message");
+            ("help", "produce help message")
+            ("city", boost::program_options::value<std::string>(), "city whose weather is to be displayed.")
+            ("country", "country whose weather to be displayed");
 
     boost::program_options::variables_map map;
     boost::program_options::store(boost::program_options::parse_command_line(argc, argv, description), map);
