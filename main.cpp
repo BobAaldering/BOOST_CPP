cc4142bbea0b32cfab2eeda7bbdbde7774f21883
Bob Aaldering <492152@student.saxion.nl>
2021-11-23 12:42:56 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <boost/program_options.hpp>
+
+int main(int argc, char* argv[]) {
+    boost::program_options::options_description description("Allowed options");
+    description.add_options()
+            ("help", "produce help message");
+
+    boost::program_options::variables_map map;
+    boost::program_options::store(boost::program_options::parse_command_line(argc, argv, description), map);
+    boost::program_options::notify(map);
+
+    if (map.count("help"))
+        std::cout << description << std::endl;
+
+    return 0;
+}
