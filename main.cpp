48fbab8d35e1314a45322b921c91c5743be25317
Bob Aaldering <74597240+BobAaldering@users.noreply.github.com>
2021-11-25 20:39:24 +0100

    Added option parsing functionality through 'Boost' facilities.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
-#include <boost/program_options.hpp>
+
+#include "option_parser.h"
 
 int main(int argc, char* argv[]) {
-    boost::program_options::options_description description("Allowed options");
-    description.add_options()
-            ("help", "produce help message")
-            ("city", boost::program_options::value<std::string>(), "city whose weather is to be displayed.")
-            ("country", "country whose weather to be displayed");
+    option_parser parser{argc, argv};
 
-    boost::program_options::variables_map map;
-    boost::program_options::store(boost::program_options::parse_command_line(argc, argv, description), map);
-    boost::program_options::notify(map);
+    parser.with_help("help_desc", "prints help diagnostics");
+    parser.add_string_positional("host_name", "host name for the server to get the HTTP response from");
+    parser.add_int_positional("port_number", "port number for the server to get the HTTP response from", 80);
+    parser.add_string_positional("target", "target character for the server to get the HTTP response from", "/");
+    parser.add_dbl_positional("http_version", "the HTTP version that is used for the HTTP request", 1.1);
 
-    if (map.count("help"))
-        std::cout << description << std::endl;
+    auto parsed_object = parser.parse_arguments();
+
+    if (!parsed_object.empty()) {
+        std::cout << "host name: " << parsed_object["host_name"].as<std::string>() << std::endl;
+        std::cout << "port number: " << parsed_object["port_number"].as<int>() << std::endl;
+        std::cout << "target: " << parsed_object["target"].as<std::string>() << std::endl;
+        std::cout << "HTTP version: " << parsed_object["http_version"].as<double>() << std::endl;
+    }
 
     return 0;
 }
---
+++ option_parser.cpp
@@ -0,0 +1,161 @@
+#include "option_parser.h"
+
+option_parser::option_parser(int arguments_count, char* arguments_value[]) :
+    m_options_description{},
+    m_positional_description{},
+    m_options_map{},
+    m_positional_count{},
+    m_help_flag{},
+    m_arguments_value{arguments_value},
+    m_arguments_count{arguments_count}
+{}
+
+void option_parser::with_help(const std::string &help_flag, const std::string &description) {
+    m_help_flag = help_flag;
+
+    m_options_description.add_options()
+        (help_flag.c_str(), description.c_str());
+}
+
+[[maybe_unused]] void option_parser::add_flag_option(const std::string &long_flag, const std::string &description) {
+    auto complete_description = description + " [bool]";
+    m_options_description.add_options()
+        (long_flag.c_str(), boost::program_options::value<bool>(), complete_description.c_str());
+}
+
+[[maybe_unused]] void option_parser::add_string_option(const std::string &long_flag, const std::string &description) {
+    auto complete_description = description + " [string]";
+    m_options_description.add_options()
+        (long_flag.c_str(), boost::program_options::value<std::string>(), complete_description.c_str());
+}
+
+[[maybe_unused]] void option_parser::add_int_option(const std::string &long_flag, const std::string &description) {
+    auto complete_description = description + " [int]";
+    m_options_description.add_options()
+        (long_flag.c_str(), boost::program_options::value<int>(), complete_description.c_str());
+}
+
+[[maybe_unused]] void option_parser::add_dbl_option(const std::string &long_flag, const std::string &description) {
+    auto complete_description = description + " [double]";
+    m_options_description.add_options()
+        (long_flag.c_str(), boost::program_options::value<double>(), complete_description.c_str());
+}
+
+[[maybe_unused]] void option_parser::add_flag_option(const std::string &long_flag, const std::string &description, bool default_value) {
+    auto complete_description = description + " [bool] - DEFAULT: " + ((default_value) ? "true" : "false");
+    m_options_description.add_options()
+        (long_flag.c_str(), boost::program_options::value<bool>()->default_value(default_value), complete_description.c_str());
+}
+
+[[maybe_unused]] void option_parser::add_string_option(const std::string &long_flag, const std::string &description, const std::string &default_value) {
+    auto complete_description = description + " [string] - DEFAULT: " + default_value;
+    m_options_description.add_options()
+        (long_flag.c_str(), boost::program_options::value<std::string>()->default_value(default_value), complete_description.c_str());
+}
+
+[[maybe_unused]] void option_parser::add_int_option(const std::string &long_flag, const std::string &description, int default_value) {
+    auto complete_description = description + " [int] - DEFAULT: " + std::to_string(default_value);
+    m_options_description.add_options()
+        (long_flag.c_str(), boost::program_options::value<int>()->default_value(default_value), complete_description.c_str());
+}
+
+[[maybe_unused]] void option_parser::add_dbl_option(const std::string &long_flag, const std::string &description, double default_value) {
+    auto complete_description = description + " [double] - DEFAULT: " + std::to_string(default_value);
+    m_options_description.add_options()
+        (long_flag.c_str(), boost::program_options::value<double>()->default_value(default_value), complete_description.c_str());
+}
+
+[[maybe_unused]] void option_parser::add_string_positional(const std::string &long_flag, const std::string &description) {
+    auto complete_description = description + " [string]";
+    m_options_description.add_options()
+        (long_flag.c_str(), boost::program_options::value<std::string>(), complete_description.c_str());
+
+    m_positional_description.add(long_flag.c_str(), 1);
+    m_positional_count++;
+}
+
+[[maybe_unused]] void option_parser::add_int_positional(const std::string &long_flag, const std::string &description) {
+    auto complete_description = description + " [int]";
+    m_options_description.add_options()
+        (long_flag.c_str(), boost::program_options::value<int>(), complete_description.c_str());
+
+    m_positional_description.add(long_flag.c_str(), 1);
+    m_positional_count++;
+}
+
+[[maybe_unused]] void option_parser::add_dbl_positional(const std::string &long_flag, const std::string &description) {
+    auto complete_description = description + " [double]";
+    m_options_description.add_options()
+        (long_flag.c_str(), boost::program_options::value<double>(), complete_description.c_str());
+
+    m_positional_description.add(long_flag.c_str(), 1);
+    m_positional_count++;
+}
+
+void option_parser::add_string_positional(const std::string &long_flag, const std::string &description, const std::string &default_value) {
+    auto complete_description = description + " [string] - DEFAULT: " + default_value;
+    m_options_description.add_options()
+            (long_flag.c_str(), boost::program_options::value<std::string>()->default_value(default_value), complete_description.c_str());
+
+    m_positional_description.add(long_flag.c_str(), 1);
+    m_positional_count++;
+}
+
+void option_parser::add_int_positional(const std::string &long_flag, const std::string &description, int default_value) {
+    auto complete_description = description + " [int] - DEFAULT: " + std::to_string(default_value);
+    m_options_description.add_options()
+            (long_flag.c_str(), boost::program_options::value<int>()->default_value(default_value), complete_description.c_str());
+
+    m_positional_description.add(long_flag.c_str(), 1);
+    m_positional_count++;
+}
+
+void option_parser::add_dbl_positional(const std::string &long_flag, const std::string &description, double default_value) {
+    auto complete_description = description + " [double] - DEFAULT: " + std::to_string(default_value);
+    m_options_description.add_options()
+            (long_flag.c_str(), boost::program_options::value<double>()->default_value(default_value), complete_description.c_str());
+
+    m_positional_description.add(long_flag.c_str(), 1);
+    m_positional_count++;
+}
+
+[[maybe_unused]] const boost::program_options::variables_map &option_parser::parse_arguments() {
+    try {
+        boost::program_options::store(boost::program_options::command_line_parser(m_arguments_count, m_arguments_value)
+            .options(m_options_description)
+            .positional(m_positional_description)
+            .run(), m_options_map);
+
+        boost::program_options::notify(m_options_map);
+    }
+    catch (boost::program_options::error& error_message) {
+        std::cerr << "ERROR MESSAGE: " << error_message.what() << std::endl;
+    }
+
+    if (m_options_map.count(m_help_flag))
+        std::cout << usage_of_options() << std::endl;
+
+    return m_options_map;
+}
+
+[[maybe_unused]] std::string option_parser::usage_of_options() const {
+    std::stringstream string_stream{};
+    string_stream << "Usage: ./" << program_invocation_short_name << " ";
+
+    if (!m_options_description.options().empty()) {
+        string_stream << "[FLAGS] ";
+
+        if (m_positional_count) {
+            for (int i = 0; i < m_positional_count; i++)
+                string_stream << '<' << m_positional_description.name_for_position(i) << "> ";
+        }
+
+        string_stream << '\n';
+
+        for (const auto& single_option : m_options_description.options()) {
+            string_stream << std::quoted(single_option->long_name()) << "\t\t" << single_option->description() << std::endl;
+        }
+    }
+
+    return string_stream.str();
+}
